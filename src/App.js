7531dbd9ace0c50584482ff0c9e7988a75265d16
Thomas-j1 <thomsjoh@ifi.uio.no>
2021-12-20 22:08:43 +0100

    Functionality for removing Tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      text: 'test1',
+      day: 'jan 4th at 2:40pm',
+      reminder: false
+    },
+    {
+      id: 1,
+      text: 'test2',
+      day: 'jan 4th at 2:40pm',
+      reminder: false
+    },
+  ])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header title='Task Tracker'/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} 
+        onDelete={deleteTask} /> : 'No Tasks yet!' }
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import Button from "./Button"
 
 const header = ({title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add'/>
+            <Button color='green' text='Add' onClick = {onClick}
+            />
         </header>
     )
 }
