3ab3c1986f1820d3702b2e3d817c92472b8adfed
Thomas-j1 <thomsjoh@ifi.uio.no>
2021-12-23 03:33:59 +0100

    Routing and json server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import Button from "./Button"
+import { useLocation } from "react-router-dom"
 
-const header = ({ title, onAdd, showAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button 
-            color={showAdd ? 'Red' : 'Green'} 
-            text={showAdd ? 'Close' : 'Add'} 
-            onClick={onAdd}
-            />
+            {location.pathname === '/' && (
+                <Button
+                    color={showAdd ? 'Red' : 'Green'}
+                    text={showAdd ? 'Close' : 'Add'}
+                    onClick={onAdd}
+                />
+            )}
         </header>
     )
 }
 
-export default header
+export default Header
